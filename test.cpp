c180a8a8746bd9817d62166f3a30bd7b1c753673
ParkSungEun <tjddms5936@naver.com>
2022-07-19 19:31:13 +0900

    text commit

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+template <typename T>
+class dynamic_array{
+    // 
+    T* data;
+    size_t n;
+}
+
+
+int main(){
+    
+}
